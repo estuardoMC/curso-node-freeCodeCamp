c25787303b8caf5a1431bb1b3765217c677c1330
estuardoMC <estuarditochez@gmail.com>
2022-08-25 17:26:00 -0600

    como importar y exportar correctamente en node

--- modulos/app.js
+++ modulos/app.js
@@ -1,2 +1,5 @@
-import {saludar} from "./saludo";
+/*importar - exportar en node: en node nosotros para exportar una funcion utilizamoc la funcion require()
+entonces desestructuramos lo que viene de require que es la funcion que estamos importando
+desestructuramos la funcion saludar que viene de requiere(dirreccion de modulo)*/
+const {saludar} = require("./saludo.js");
 console.log(saludar("Estuardo"));
--- modulos/saludo.js
+++ modulos/saludo.js
@@ -1,3 +1,8 @@
-export const saludar = (nombre)=>{
+/*en node para exportar una funcion lo hacemos mediante el objeto module.exports*/
+const saludar = (nombre)=>{
     return `Hola ${nombre}`;
+}
+
+module.exports = {
+    saludar
 }
